428c3305e21cae6beac318e05cf221774de6fbf1
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-02-03 16:46:18 +0000

    exploration of seq assay ids

---
+++ 2022-02-02_celeste_seq_assay_ids.R
@@ -0,0 +1,70 @@
+# Description: Check seq assay ids.
+# Author: Haley Hunter-Zinck
+# Date: 2022-02-02
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(yaml)
+library(synapser)
+synLogin()
+
+# synapse
+synid_file_sam <- "syn7229822"
+synid_file_assay <- "syn18134830"
+synid_table_sam <- "syn7517674"
+
+# functions ----------------------------
+
+#' Download and load data stored in csv or other delimited format on Synapse
+#' into an R data frame.
+#' 
+#' @param synapse_id Synapse ID
+#' @version Version of the Synapse entity to download.  NA will load current
+#' version
+#' @param set Delimiter for file
+#' @param na.strings Vector of strings to be read in as NA values
+#' @param header TRUE if the file contains a header row; FALSE otherwise.
+#' @param check_names TRUE if column names should be modified for compatibility 
+#' with R upon reading; FALSE otherwise.
+#' @param comment.char character designating comment lines to ignore
+#' @return data frame
+get_synapse_entity_data_in_csv <- function(synapse_id, 
+                                           version = NA,
+                                           sep = ",", 
+                                           na.strings = c("NA"), 
+                                           header = T,
+                                           check_names = F,
+                                           comment.char = "#") {
+  
+  if (is.na(version)) {
+    entity <- synGet(synapse_id)
+  } else {
+    entity <- synGet(synapse_id, version = version)
+  }
+  
+  data <- read.csv(entity$path, stringsAsFactors = F, 
+                   na.strings = na.strings, sep = sep, check.names = check_names,
+                   header = header, comment.char = comment.char)
+  return(data)
+}
+
+# read ----------------------------
+
+sam <- get_synapse_entity_data_in_csv(synid_file_sam, sep = "\t")
+assay <- read_yaml(synGet(synid_file_assay)$path)
+
+# main ----------------------------
+
+sam_id <- as.character(unlist(sam %>% select(SEQ_ASSAY_ID) %>% distinct() %>% arrange(SEQ_ASSAY_ID)))
+assay_id <- c(names(assay)[1:4], unlist(lapply(assay$`UHN-555`$assay_specific_info, function(x) {x$SEQ_ASSAY_ID})))
+
+setdiff(assay_id, sam_id)
+
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
