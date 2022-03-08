c01aed0310138cbf2e311eb59f69267e04b4e824
Haley Hunter-Zinck <haley.hunterzinck@sagebase.org>
2022-03-08 08:38:07 -0800

    add phase 2 bpc upload folders and annotations

---
+++ 2022-03-08_haley_create_phase_2_placeholders.R
@@ -0,0 +1,53 @@
+# Description: Add annotations to GENIE BPC uploads.
+# Author: Haley Hunter-Zinck
+# Date: 2022-02-17
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(synapser)
+synLogin()
+
+# synapse
+synid_folder_upload <- "syn20852283"
+#synid_folder_upload <- "syn27001905"
+
+# parameters
+cohorts <- c("OVARIAN", "ESOPHAGO", "RENAL", "MELANOMA")
+sites <- c("DFCI", "MSK", "UHN", "VICC")
+file_placeholder <- "placeholder.txt"
+
+# main ----------------------------
+
+# create placeholder files
+write(c("this is a placedholer file"), ncolumns = 1, file = file_placeholder)
+
+for (cohort in cohorts) {
+  synid_folder_cohort <- synStore(Folder(name = cohort, parent = synid_folder_upload))
+  
+  for (site in sites) {
+    synid_folder_site <- synStore(Folder(name = site, parent = synid_folder_cohort))
+    
+    if (site == "DFCI") {
+      synid_file_header <- synStore(File(path = file_placeholder, 
+                                         parent = synid_folder_site, 
+                                         name = glue("{site} {cohort} Header"),
+                                         annotations = list(cohort = cohort, site = site, file_type = "header", phase = "2")))
+    } 
+    synid_file_data <- synStore(File(path = file_placeholder, 
+                                     parent = synid_folder_site, 
+                                     name = glue("{site} {cohort} Data"),
+                                     annotations = list(cohort = cohort, site = site, file_type = "data", phase = "2")))
+  }
+}
+
+# clean up
+file.remove(file_placeholder)
+
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
