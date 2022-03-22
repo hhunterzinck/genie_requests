8372ef20c2e4a5eb3b8a0ba5c0a99bbe2396c654
Haley Hunter-Zinck <haley.hunterzinck@sagebase.org>
2022-03-21 19:44:44 -0700

    check genomic information file

---
+++ 2022-03-21_tom_check_gen_info_file.R
@@ -0,0 +1,66 @@
+# Description: 
+# Author: Haley Hunter-Zinck
+# Date: 
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(synapser)
+synLogin()
+
+# synapse
+synid_file_gen <- "syn9734427"
+synid_version_gen <- "91"
+
+# functions ----------------------------
+
+#' Download and load data stored in csv or other delimited format on Synapse
+#' into an R data frame.
+#' 
+#' @param synapse_id Synapse ID
+#' @version Version of the Synapse entity to download.  NA will load current
+#' version
+#' @param set Delimiter for file
+#' @param na.strings Vector of strings to be read in as NA values
+#' @param header TRUE if the file contains a header row; FALSE otherwise.
+#' @param check_names TRUE if column names should be modified for compatibility 
+#' with R upon reading; FALSE otherwise.
+#' @param comment.char character designating comment lines to ignore
+#' @return data frame
+get_synapse_entity_data_in_csv <- function(synapse_id, 
+                                           version = NA,
+                                           sep = ",", 
+                                           na.strings = c("NA"), 
+                                           header = T,
+                                           check_names = F,
+                                           comment.char = "#") {
+  
+  if (is.na(version)) {
+    entity <- synGet(synapse_id)
+  } else {
+    entity <- synGet(synapse_id, version = version)
+  }
+  
+  data <- read.csv(entity$path, stringsAsFactors = F, 
+                   na.strings = na.strings, sep = sep, check.names = check_names,
+                   header = header, comment.char = comment.char)
+  return(data)
+}
+
+
+# read ----------------------------
+
+gen <- get_synapse_entity_data_in_csv(synid_file_gen, sep = "\t", version = synid_version_gen)
+
+
+# main ----------------------------
+
+gen %>% group_by(clinicalReported) %>% count()
+
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
