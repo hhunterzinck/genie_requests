605b19b94247c5d4f8efcdac03f2303c4cbc291e
hhunterzinck <haley.hunterzinck@sagebase.org>
2022-04-13 22:33:30 +0000

    check record id

---
+++ 2022-04-13_shirin_prostate_upload.R
@@ -0,0 +1,72 @@
+# Description: Check for ineligible patient ID.
+# Author: Haley Hunter-Zinck
+# Date: 2022-04-13
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(synapser)
+
+# synapse
+synid_file_msk <- "syn26230493"
+
+# parameters
+id_pt <- "GENIE-MSK-P-0027255"
+
+# functions ----------------------------
+
+#' Download and load data stored in csv or other delimited format on Synapse
+#' into an R data frame.
+#' 
+#' @param synapse_id Synapse ID
+#' @version Version of the Synapse entity to download.  NA will load current
+#' version
+#' @param set Delimiter for file
+#' @param na.strings Vector of strings to be read in as NA values
+#' @param header TRUE if the file contains a header row; FALSE otherwise.
+#' @param check_names TRUE if column names should be modified for compatibility 
+#' with R upon reading; FALSE otherwise.
+#' @param comment.char character designating comment lines to ignore
+#' @return data frame
+get_synapse_entity_data_in_csv <- function(synapse_id, 
+                                           version = NA,
+                                           sep = ",", 
+                                           na.strings = c("NA"), 
+                                           header = T,
+                                           check_names = F,
+                                           comment.char = "#",
+                                           colClasses = "character") {
+  
+  if (is.na(version)) {
+    entity <- synGet(synapse_id)
+  } else {
+    entity <- synGet(synapse_id, version = version)
+  }
+  
+  data <- read.csv(entity$path, stringsAsFactors = F, 
+                   na.strings = na.strings, sep = sep, check.names = check_names,
+                   header = header, comment.char = comment.char, colClasses = colClasses)
+  return(data)
+}
+
+# synapse login --------------------
+
+synLogin()
+
+# read ----------------------------
+
+data <- get_synapse_entity_data_in_csv(synid_file_msk)
+
+# main ----------------------------
+
+data %>% 
+  filter(record_id == id_pt) %>%
+  select(record_id, redcap_repeat_instrument, redcap_repeat_instance)
+  
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
